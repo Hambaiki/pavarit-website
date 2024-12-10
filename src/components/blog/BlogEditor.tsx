a59dc708d8ae08cc7022ba19b0daf117c8e68798
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2024-12-10 14:05:20 +0700

    Fix build issue with removed library

--- src/components/blog/BlogEditor.tsx
+++ src/components/blog/BlogEditor.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from "react";
-import ReactQuill from "react-quill-new";
 
 import "react-quill-new/dist/quill.snow.css";
 
@@ -19,13 +18,13 @@ const BlogEditor = ({ onSave }: BlogEditorProps) => {
   return (
     <div className="flex flex-col items-center w-full max-w-4xl mx-auto mt-10 shadow-lg rounded-lg">
       <h1 className="text-2xl font-bold mb-4">Write Your Blog</h1>
-      <ReactQuill
+      {/* <ReactQuill
         className="w-full"
         theme="snow"
         value={content}
         onChange={setContent}
         placeholder="Write your article here..."
-      />
+      /> */}
       <button
         className="mt-4 px-6 py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600"
         onClick={handleSave}
