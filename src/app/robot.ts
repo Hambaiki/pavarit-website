77130fab532e01672d2fb2f3224fd5163a54df2f
Hambaiki <92420824+Hambaiki@users.noreply.github.com>
2024-12-17 11:36:17 +0700

    Add robot.ts for generating robot.txt

---
+++ src/app/robot.ts
@@ -0,0 +1,13 @@
+import { MetadataRoute } from "next";
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+      // Disallowed paths
+      disallow: ["/profile/", "/cms/"],
+    },
+    sitemap: `${process.env.NEXT_PUBLIC_BASE_URL}/sitemap.xml`,
+  };
+}
